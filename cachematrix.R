d090aecd2a86f295aaf2a38acbf8972ce9bb0c97
Shannon Rhodes <srhodes-phd@users.noreply.github.com>
2015-06-15 13:45:06 -0700

    Code added to shell program

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+            v <- NULL
+            set <- function(y) {
+                  x <<- y
+                  v <<- NULL
+            }
+            get <- function() x
+            setinv <- function(solve) v <<- solve
+            getinv <- function() v
+            list(set = set, get = get,
+                 setinv = setinv,
+                 getinv = getinv)
+      }
 
-}
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+      v <- x$getinv()
+      if(!is.null(v)) {
+            message("getting cached data")
+            return(v)
+      }
+      data <- x$get()
+      v <- solve(data, ...)
+      x$setinv(v)
+      v
 }
